d6e83017f70b138c6546996a263563c1b21459a8
Ahmed Ali <ahmedaliaay@gmail.com>
2019-02-22 14:48:14 +0200

    Build Collapse class

--- src/model/disasters/Collapse.java
+++ src/model/disasters/Collapse.java
@@ -4,7 +4,27 @@ import model.infrastructure.ResidentialBuilding;
 import simulation.Rescuable;
 
 public class Collapse  {
-
-
+	private int cycle;
+	private ResidentialBuilding target;
+	public Collapse(int cycle, ResidentialBuilding target) {
+		this.cycle = cycle;
+		this.target = target;
+	}
+	public int getCycle()
+	{
+		return cycle;
+	}
+	public void setCycle(int cycle)
+	{
+		this.cycle = cycle;
+	}
+	public ResidentialBuilding getTarget()
+	{
+		return target;
+	}
+	public void setTarget(ResidentialBuilding target)
+	{
+		this.target = target;
+	}
 
 }
