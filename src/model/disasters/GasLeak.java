1bd66f82628a37f44209051afba433632e327bc5
Ahmed Ali <ahmedaliaay@gmail.com>
2019-02-22 14:53:55 +0200

    Create GasLeak class

---
+++ src/model/disasters/GasLeak.java
@@ -0,0 +1,11 @@
+package model.disasters;
+
+import model.infrastructure.ResidentialBuilding;
+
+public class GasLeak extends Disaster {
+
+	public GasLeak(int startCycle, ResidentialBuilding target) {
+		super(startCycle, target);
+	}
+
+}
