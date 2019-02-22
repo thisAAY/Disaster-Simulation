a3a29b2c3271f47e123db100de7a98489983dd9a
Ahmed Ali <ahmedaliaay@gmail.com>
2019-02-22 14:51:10 +0200

    Make collapse extends disaster

--- src/model/disasters/Collapse.java
+++ src/model/disasters/Collapse.java
@@ -3,28 +3,10 @@ package model.disasters;
 import model.infrastructure.ResidentialBuilding;
 import simulation.Rescuable;
 
-public class Collapse  {
-	private int cycle;
-	private ResidentialBuilding target;
+public class Collapse extends Disaster {
 	public Collapse(int cycle, ResidentialBuilding target) {
-		this.cycle = cycle;
-		this.target = target;
+		super(cycle,target);
 	}
-	public int getCycle()
-	{
-		return cycle;
-	}
-	public void setCycle(int cycle)
-	{
-		this.cycle = cycle;
-	}
-	public ResidentialBuilding getTarget()
-	{
-		return target;
-	}
-	public void setTarget(ResidentialBuilding target)
-	{
-		this.target = target;
-	}
+	
 
 }
