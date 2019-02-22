53cba85e0298000f5e12c4f314c910c6607db6c6
Ahmed Ali <ahmedaliaay@gmail.com>
2019-02-22 15:09:35 +0200

    Make ResBuilding variables private

--- src/model/infrastructure/ResidentialBuilding.java
+++ src/model/infrastructure/ResidentialBuilding.java
@@ -8,46 +8,60 @@ import simulation.Address;
 import simulation.Rescuable;
 import simulation.Simulatable;
 
-public class ResidentialBuilding implements Rescuable,Simulatable {
-	Address location;
-	int structuralIntegrity=100;
-	int fireDamage=0;
-	int gasLevel=0;
-	int foundationDamage=0;
-	ArrayList<Citizen> occupants;
-	Disaster disaster;
-	public ResidentialBuilding(Address location)
-	{
-		this.location=location;
+public class ResidentialBuilding implements Rescuable, Simulatable {
+	private Address location;
+	private int structuralIntegrity = 100;
+	private int fireDamage = 0;
+	private int gasLevel = 0;
+	private int foundationDamage = 0;
+	private ArrayList<Citizen> occupants;
+	private Disaster disaster;
+
+	public ResidentialBuilding(Address location) {
+		this.location = location;
 	}
-public Address getLocation(){
-	return location;
-}
-public Disaster getDisaster (){
-	return disaster;
-}
-public ArrayList<Citizen> getOccupants(){
-	return occupants;
-}
-public int getStructuralIntegrity(){
-	return structuralIntegrity;
-}
-public void setStructuralIntegrity(int s){
-	structuralIntegrity=s;
-}
-public int getFireDamage(){
-	return fireDamage;
-}
-public void setFireDamage(int f){
-	fireDamage=f;
-}
-public int getGasDamage(){
-	return gasLevel;
-}
-public void setGasDamage(int g){gasLevel=g;}public int getFoundationDamage(){
-	return foundationDamage;
-}
-public void setFoundationDamage(int fd){
-	foundationDamage=fd;
-}
+
+	public Address getLocation() {
+		return location;
+	}
+
+	public Disaster getDisaster() {
+		return disaster;
+	}
+
+	public ArrayList<Citizen> getOccupants() {
+		return occupants;
+	}
+
+	public int getStructuralIntegrity() {
+		return structuralIntegrity;
+	}
+
+	public void setStructuralIntegrity(int s) {
+		structuralIntegrity = s;
+	}
+
+	public int getFireDamage() {
+		return fireDamage;
+	}
+
+	public void setFireDamage(int fireDamage) {
+		this.fireDamage = fireDamage;
+	}
+
+	public int getGasDamage() {
+		return gasLevel;
+	}
+
+	public void setGasDamage(int gasDamage) {
+		this.gasLevel = gasDamage;
+	}
+
+	public int getFoundationDamage() {
+		return foundationDamage;
+	}
+
+	public void setFoundationDamage(int foundationDamage) {
+		this.foundationDamage = foundationDamage;
+	}
 }
