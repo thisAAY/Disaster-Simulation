7d5841f0ff756f8f497c85c6b6cec88ff06d21c6
Ahmed Ali <ahmedaliaay@gmail.com>
2019-02-22 15:28:38 +0200

    Make MedicalUnit class var private and create getters and setters

--- src/model/units/MedicalUnit.java
+++ src/model/units/MedicalUnit.java
@@ -3,11 +3,28 @@ package model.units;
 import simulation.Address;
 
 public abstract class MedicalUnit extends Unit{
-	int healingAmount=10;
-	int treatmentAmount=10;
+	private int healingAmount;
+	private int treatmentAmount;;
 	public MedicalUnit(String id, Address location, int stepsPerCycle){
 		super(id,location,stepsPerCycle);
+		healingAmount = 10;
+		treatmentAmount = 10;
 	}
 	
-
+	public int getHealingAmount()
+	{
+		return healingAmount;
+	}
+	public void setHealingAmount(int healingAmount)
+	{
+		this.healingAmount = healingAmount;
+	}
+	public int getTreatmentAmount()
+	{
+		return treatmentAmount;
+	}
+	public void setTreatmentAmount(int treatmentAmount)
+	{
+		this.treatmentAmount = treatmentAmount;
+	}
 }
